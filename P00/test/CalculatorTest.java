7b7c7f07a6ef613b0e2e7f1b5072f1e405569fd2
22038069 <22038069@MSI>
2023-05-25 11:55:56 +0800

    First commit

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	public final void testAdd() {
+		//fail("Not yet implemented"); //T0D0
+		int a = 12344;
+		int b = 8765;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a,b);
+		
+		int expected = 9999;
+		assertEquals (expected,actual);
+	}
+	
+	@Test
+	public final void testSubtract() {
+		//fail("Not yet implemented"); //T0D0
+		int a = 9876;
+		int b = 4321;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.subtract(a,b);
+		
+		int expected = 5555;
+		assertEquals(actual, expected);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
